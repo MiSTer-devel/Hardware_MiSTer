07496a02274435b947067346371a894393dcaf31
sorgelig <pour.garbage@gmail.com>
2017-06-28 16:13:14 +0800

    Add clean.bat.

---
+++ Addons/clean.bat
@@ -0,0 +1,7 @@
+@echo off
+del debug.log
+for /d %%p in (*) do for /d %%n in (%%p\Project*) do rmdir /s /q "%%n"
+for /d %%p in (*) do rmdir /s /q "%%p\History"
+for /d %%p in (*) do rmdir /s /q "%%p\__Previews"
+
+pause
